bfd526f4921f2a387c49d38287ecc8cb04c1cddf
Renato Ferreira <renatofdds@gmail.com>
2020-05-07 06:48:18 -0300

    Grey

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil
+	(projectile-project-compilation-cmd . "./autogen.sh --disable-cinnamon --disable-gnome-shell --disable-metacity --disable-plank --disable-unity --disable-xfwm --prefix=/home/user/.local && ./configure --disable-cinnamon --disable-gnome-shell --disable-metacity --disable-plank --disable-unity --disable-xfwm --prefix=/home/user/.local && make -j && make install")))
